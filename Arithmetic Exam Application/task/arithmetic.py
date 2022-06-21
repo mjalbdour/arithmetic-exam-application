bdef06acb1b4bd3121c75f33aa98122fe2ccbf83
Mohammad Al Bdour <mjalbdour@gmail.com>
2022-06-22 01:38:46 +0300

    Stage 1/4: Mini-calculator

--- Arithmetic Exam Application/task/arithmetic.py
+++ Arithmetic Exam Application/task/arithmetic.py
@@ -1,1 +1,12 @@
 # write your code here
+
+op_func = {
+    "+": lambda x, y: x + y,
+    "-": lambda x, y: x - y,
+    "*": lambda x, y: x * y
+}
+
+
+num1, op, num2 = input().split()
+
+print(op_func[op](int(num1), int(num2)))
