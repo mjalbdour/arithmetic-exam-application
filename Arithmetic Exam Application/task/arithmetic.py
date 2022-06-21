b3ff68f337ad3d1acf74d962489ed9ffbeaa8689
Mohammad Al Bdour <mjalbdour@gmail.com>
2022-06-22 01:50:37 +0300

    Stage 2/4: Task generator

--- Arithmetic Exam Application/task/arithmetic.py
+++ Arithmetic Exam Application/task/arithmetic.py
@@ -1,4 +1,5 @@
 # write your code here
+import random
 
 op_func = {
     "+": lambda x, y: x + y,
@@ -7,6 +8,24 @@ op_func = {
 }
 
 
-num1, op, num2 = input().split()
+def generate_task():
+    x = random.randint(2, 9)
+    y = random.randint(2, 9)
+    op = random.choice([k for k in op_func.keys()])
+    return f'{x} {op} {y}'
 
-print(op_func[op](int(num1), int(num2)))
+
+def check_answer(answer, task):
+    answer = int(answer)
+    task = task.split()
+    x = int(task[0])
+    op = task[1]
+    y = int(task[2])
+    return answer == op_func[op](x, y)
+
+
+generated_task = generate_task()
+print(generated_task)
+user_answer = input()
+result = check_answer(user_answer, generated_task)
+print("Right!") if result else print("Wrong!")
